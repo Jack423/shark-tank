45a5fcab4e2d581c795067b1e5dc740bdd12c6b5
GitHub <noreply@github.com>
2019-02-21 16:20:04 -0600

    Added getRandomNumberInRange, printArray, and changeData methods to class.

--- src/com/apexsoftware/Main.java
+++ src/com/apexsoftware/Main.java
@@ -1,33 +1,67 @@
 package com.apexsoftware;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
         String filePath = "C:\\Users\\Jack\\OneDrive\\FERC\\Shark Tank\\data8-9.csv";
         File file = new File(filePath);
 
-        List<List<String>> lines = new ArrayList<>();
         Scanner inputStream;
+        Integer[][] data = new Integer[192][9];
+        String delimiter = ",";
+        int index = 0;
 
         try {
             inputStream = new Scanner(file);
 
-            while (inputStream.hasNext()) {
-                String line = inputStream.next();
-                String[] values = line.split(",");
-                lines.add(Arrays.asList(values));
+            for (int i = 0; inputStream.hasNext(); i++) {
+                String line = inputStream.nextLine();
+                String[] dataAsString = line.split(delimiter);
+                Integer[] dataAsInt = new Integer[dataAsString.length];
+
+                for (int j = 0; j < dataAsString.length; j++) {
+                    dataAsInt[j] = Integer.parseInt(dataAsString[j]);
+                }
+                data[i] = dataAsInt;
             }
 
             inputStream.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        data = changeData(data);
+        printArray(data);
+    }
+
+    private static Integer[][] changeData(Integer[][] data) {
+        for (int i = 0; i < 192; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (j <= 3) {
+                    if (data[i][j] == 7) {
+                        data[i][j] = getRandomNumberInRange(5, 7);
+                    } else if (data[i][j] == 1) {
+                        data[i][j] = getRandomNumberInRange(1, 3);
+                    }
+
+                    data[i][j] = getRandomNumberInRange((data[i][j] - 1), data[i][j] + 1);
+                } else {
+                    if (data[i][j] == 10) {
+                        data[i][j] = getRandomNumberInRange(8, 10);
+                    } else if (data[i][j] == 1) {
+                        data[i][j] = getRandomNumberInRange(1, 3);
+                    }
+                }
+            }
+        }
+
+        return data;
     }
 
     private static int getRandomNumberInRange(int min, int max) {
@@ -37,5 +71,18 @@ public class Main {
 
         Random random = new Random();
         return random.nextInt((max - min) + 1) + min;
+    }
+
+    private static void printArray(Integer[][] data) {
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                if (j == 8) {
+                    System.out.print(data[i][j]);
+                } else {
+                    System.out.print(data[i][j] + ",");
+                }
+            }
+            System.out.println();
+        }
     }
 }
